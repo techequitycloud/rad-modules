7050c714a32cc7431254479ffaae7568634f017c
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2025-10-20 20:46:13 +0100

    Update main.tf

--- modules/AWS_EKS/main.tf
+++ modules/AWS_EKS/main.tf
@@ -102,9 +102,9 @@ resource "aws_eks_node_group" "node" {
   subnet_ids      = aws_subnet.public[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 3
-    min_size     = 1
+    desired_size = 2
+    max_size     = 5
+    min_size     = 2
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
