3165b2ccf8c02e1d5b5ee8d57725f8609c9ef591
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2025-11-06 12:25:32 +0000

    Update variables.tf

--- modules/Cloudrun_Odoo/variables.tf
+++ modules/Cloudrun_Odoo/variables.tf
@@ -50,7 +50,7 @@ variable "credit_cost" {
 variable "require_credit_purchases" {
   description = "Set to true to require credit purchases to deploy this module. {{UIMeta group=0 order=104 }}"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "deployment_id" {
