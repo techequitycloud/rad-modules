5217bf5a979112ec0f6bbe9e5189f6ff23a29197
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2025-11-06 09:58:50 +0000

    Update default values for credit cost and purchases

--- modules/GCP_Services/variables.tf
+++ modules/GCP_Services/variables.tf
@@ -44,13 +44,13 @@ variable "module_services" {
 variable "credit_cost" {
   description = "Specify the module cost {{UIMeta group=0 order=103 }}"
   type        = number
-  default     = 200
+  default     = 500
 }
 
 variable "require_credit_purchases" {
   description = "Set to true to require credit purchases to deploy this module. {{UIMeta group=0 order=104 }}"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "deployment_id" {
