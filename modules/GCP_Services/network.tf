29fc49e3b468b542fd884786e31e99aa543585dd
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2025-11-11 17:26:00 +0000

    Update network.tf

--- modules/GCP_Services/network.tf
+++ modules/GCP_Services/network.tf
@@ -34,7 +34,6 @@ resource "google_compute_network" "vpc_network" {
 
   depends_on   = [
     null_resource.api_poll,
-    google_service_account.project_sa_admin,
     google_service_account.cloud_run_sa_admin,
     google_service_account.cloud_build_sa_admin,
     google_service_account.cloud_deploy_sa_admin,
