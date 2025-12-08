48934d5b08f70385ee2409def0858cae25337b2f
Shiyghan Navti <shiyghan.navti@techequity.cloud>
2025-12-08 14:51:24 +0000

    Updated N8N and Odoo modules following extensive testing

--- modules/Odoo/importdb.tf
+++ modules/Odoo/importdb.tf
@@ -116,8 +116,7 @@ resource "null_resource" "import_dev_db" {
 
       # Ensure application directory is empty and execute the script
       for i in {1..5}; do
-        if [ -z "${local.project_sa_email}" ]; 
-        then
+        if [ -z "${local.project_sa_email}" ] || [ -z "${var.resource_creator_identity}" ]; then 
           if gcloud compute ssh --project ${local.project.project_id} --quiet $NFS_VM --zone ${data.google_compute_zones.available_zones.names[0]} --command="sudo bash -s" < ${path.module}/scripts/app/dev/import-db.sh; then
             echo "SSH command succeeded"
             break
@@ -194,8 +193,7 @@ resource "null_resource" "import_qa_db" {
 
       # Ensure application directory is empty and execute the script
       for i in {1..5}; do
-        if [ -z "${local.project_sa_email}" ]; 
-        then
+        if [ -z "${local.project_sa_email}" ] || [ -z "${var.resource_creator_identity}" ]; then 
           if gcloud compute ssh --project ${local.project.project_id} --quiet $NFS_VM --zone ${data.google_compute_zones.available_zones.names[0]} --command="sudo bash -s" < ${path.module}/scripts/app/qa/import-db.sh; then
             echo "SSH command succeeded"
             break
@@ -273,8 +271,7 @@ resource "null_resource" "import_prod_db" {
 
       # Ensure application directory is empty and execute the script
       for i in {1..5}; do
-        if [ -z "${local.project_sa_email}" ]; 
-        then
+        if [ -z "${local.project_sa_email}" ] || [ -z "${var.resource_creator_identity}" ]; then 
           if gcloud compute ssh --project ${local.project.project_id} --quiet $NFS_VM --zone ${data.google_compute_zones.available_zones.names[0]} --command="sudo bash -s" < ${path.module}/scripts/app/prod/import-db.sh; then
             echo "SSH command succeeded"
             break
--- modules/Odoo/importnfs.tf
+++ modules/Odoo/importnfs.tf
@@ -113,7 +113,7 @@ resource "null_resource" "import_dev_nfs" {
 
       # Ensure application directory is empty and execute the script
       for i in {1..5}; do
-        if [ -z "${local.project_sa_email}" ] && [ -z "${var.resource_creator_identity}" ]; then
+        if [ -z "${local.project_sa_email}" ] || [ -z "${var.resource_creator_identity}" ]; then
           if gcloud compute ssh --project ${local.project.project_id} --quiet $NFS_VM --zone ${data.google_compute_zones.available_zones.names[0]} --command="sudo bash -s" < ${path.module}/scripts/app/dev/import-nfs.sh; then
             echo "SSH command succeeded"
             break
@@ -189,7 +189,7 @@ resource "null_resource" "import_qa_nfs" {
 
       # Ensure application directory is empty and execute the script
       for i in {1..5}; do
-        if [ -z "${local.project_sa_email}" ] && [ -z "${var.resource_creator_identity}" ]; then
+        if [ -z "${local.project_sa_email}" ] || [ -z "${var.resource_creator_identity}" ]; then
           if gcloud compute ssh --project ${local.project.project_id} --quiet $NFS_VM --zone ${data.google_compute_zones.available_zones.names[0]} --command="sudo bash -s" < ${path.module}/scripts/app/qa/import-nfs.sh; then
             echo "SSH command succeeded"
             break
@@ -266,7 +266,7 @@ resource "null_resource" "import_prod_nfs" {
 
       # Ensure application directory is empty and execute the script
       for i in {1..5}; do
-        if [ -z "${local.project_sa_email}" ] && [ -z "${var.resource_creator_identity}" ]; then
+        if [ -z "${local.project_sa_email}" ] || [ -z "${var.resource_creator_identity}" ]; then
           if gcloud compute ssh --project ${local.project.project_id} --quiet $NFS_VM --zone ${data.google_compute_zones.available_zones.names[0]} --command="sudo bash -s" < ${path.module}/scripts/app/prod/import-nfs.sh; then
             echo "SSH command succeeded"
             break
