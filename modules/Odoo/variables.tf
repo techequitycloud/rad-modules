2a67b6f517d17f1e751f3525cfe418f5d49f3a36
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2025-12-15 21:21:56 +0000

    Update variables.tf

--- modules/Odoo/variables.tf
+++ modules/Odoo/variables.tf
@@ -41,7 +41,7 @@ variable "credit_cost" {
 variable "require_credit_purchases" {
   description = "Set to true to require credit purchases to deploy this module. {{UIMeta group=0 order=104 }}"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "enable_purge" {
