764d10e32c02db0a10e36ed0f953d81b16c97ade
Shiyghan Navti <shiyghan.navti@techequity.cloud>
2025-12-22 01:48:06 +0000

    Updated module_dependencies

--- modules/N8N/variables.tf
+++ modules/N8N/variables.tf
@@ -23,7 +23,7 @@ variable "module_description" {
 variable "module_dependency" {
   description = "Specify the names of the modules this module depends on in the order in which they should be deployed. {{UIMeta group=0 order=102 }}"
   type        = list(string)
-  default     = ["GCP Project","GCP Services"]
+  default     = ["GCP Services"]
 }
 
 variable "module_services" {
