f6fd5454ac2f8ffd45df5eaebd5590890e0d91ca
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2026-01-08 21:32:56 +0000

    Update iam.tf

--- modules/GCP_Project/iam.tf
+++ modules/GCP_Project/iam.tf
@@ -54,5 +54,5 @@ resource "google_project_iam_member" "devops_permissions" {
 resource "google_service_account_iam_member" "rad_agent_impersonation" {
   service_account_id = google_service_account.rad_agent.id
   role               = "roles/iam.serviceAccountTokenCreator"
-  member             = "serviceAccount:rad-module-creator@tec-rad-ui-2b65.iam.gserviceaccount.com"
+  member             = var.resource_creator_identity
 }
