846d11e907fbeb1a9f5343fc3d561e792c92fc10
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2026-01-09 13:32:48 +0000

    Update importdb.tf

--- modules/Cyclos/importdb.tf
+++ modules/Cyclos/importdb.tf
@@ -96,6 +96,7 @@ resource "null_resource" "import_db" {
           --zone ${data.google_compute_zones.available_zones.names[0]} \
           $IMPERSONATE_FLAG \
           --tunnel-through-iap \
+          --no-oslogin \
           --command="sudo bash -s" < ${path.module}/scripts/app/import-db.sh; then
           echo "SSH command succeeded"
           break
@@ -122,5 +123,6 @@ resource "null_resource" "import_db" {
     google_project_iam_member.impersonation_iap_tunnel,
     google_project_iam_member.impersonation_sa_user,
     google_project_iam_member.impersonation_compute_viewer,
+    google_project_iam_member.impersonation_os_admin_login,
   ]
 }
