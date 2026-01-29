f47d7ea134d5d4847379edd934a0431d9782811b
Shiyghan Navti <shiyghan.navti@techequity.cloud>
2026-01-29 16:00:15 +0000

    Updated modules

--- modules/Wordpress/wordpress.tf
+++ modules/Wordpress/wordpress.tf
@@ -1,12 +1,12 @@
 locals {
   wordpress_module = {
-    app_name        = "wp"
-    description     = "WordPress CMS - Popular content management system for websites and blogs"
-    container_image = "wordpress"
-    container_port  = 80
-    database_type   = "MYSQL_8_0"
-    db_name         = "wp"
-    db_user         = "wp"
+    app_name            = "wp"
+    description         = "WordPress CMS - Popular content management system for websites and blogs"
+    container_image     = "wordpress"
+    container_port      = 80
+    database_type       = "MYSQL_8_0"
+    db_name             = "wp"
+    db_user             = "wp"
     application_version = var.application_version
     application_sha     = "52d5f05c96a9155f78ed84700264307e5dea14b4"
 
@@ -17,7 +17,7 @@ locals {
     container_build_config = {
       enabled            = true
       dockerfile_path    = "Dockerfile"
-      context_path       = "."
+      context_path       = "wordpress"
       dockerfile_content = null
       build_args         = {}
       artifact_repo_name = null
@@ -45,7 +45,7 @@ locals {
 
     # Environment variables
     environment_variables = {
-      WORDPRESS_DB_HOST      = "localhost:/var/run/mysqld/mysqld.sock"
+      WORDPRESS_DB_HOST      = "localhost:/tmp/mysqld.sock"
       WORDPRESS_TABLE_PREFIX = "wp_"
       WORDPRESS_DEBUG        = "false"
     }
