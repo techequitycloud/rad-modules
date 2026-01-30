c82f688ca7b3974e920d835461c8ceda5514c55d
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2026-01-30 00:42:04 +0000

    Update variables.tf

--- modules/Ghost/variables.tf
+++ modules/Ghost/variables.tf
@@ -199,7 +199,7 @@ variable "deployment_region" {
 variable "application_version" {
   description = "Application version tag (e.g., 1.0.0, latest). {{UIMeta group=0 order=302 updatesafe }}"
   type        = string
-  default     = "5.103.1"
+  default     = "6.16.0"
 }
 
 # ===========================
