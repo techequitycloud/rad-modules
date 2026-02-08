52957607345026bc2f933b5bd6f637c8ba5568f6
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2026-02-08 15:28:48 +0200

    Update variables.tf

--- modules/GCP_Services/variables.tf
+++ modules/GCP_Services/variables.tf
@@ -87,7 +87,7 @@ variable "agent_service_account" {
 ################################################################################
 
 variable "resource_creator_identity" {
-  description = "The Service Account used by terraform to create resources in the destination project. Assign time limited conditional Basic Owner IAM role in the destination project. {{UIMeta group=1 order=101 }}"
+  description = "The Service Account used by terraform to create resources in the destination project. Assign time limited conditional Basic Owner IAM role in the destination project. {{UIMeta group=0 order=101 }}"
   type        = string
   default     = "rad-module-creator@tec-rad-ui-2b65.iam.gserviceaccount.com"
 }
