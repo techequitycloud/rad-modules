88c412f2abb66f8a1374f6f78a22075848279124
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2026-03-10 23:02:08 +0000

    Update variables.tf

--- modules/Istio_GKE/variables.tf
+++ modules/Istio_GKE/variables.tf
@@ -59,7 +59,7 @@ default = true
 }
 
 variable "resource_creator_identity" {
-  description = "The terraform Service Account used to create resources in the destination project. This Service Account must be assigned roles/owner IAM role in the destination project. {{UIMeta group=1 order=102 updatesafe }}"
+  description = "The terraform Service Account used to create resources in the destination project. This Service Account must be assigned roles/owner IAM role in the destination project. {{UIMeta group=0 order=102 updatesafe }}"
   type        = string
   default     = "rad-module-creator@tec-rad-ui-2b65.iam.gserviceaccount.com"
 }
